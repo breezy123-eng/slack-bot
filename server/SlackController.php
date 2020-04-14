f833d9bc1711907444563797cdb329ddac90897f
Brian Le <brianle@Brians-MacBook-Pro.local>
2020-04-13 22:27:30 -0700

    Added config file.

--- server/SlackController.php
+++ server/SlackController.php
@@ -8,19 +8,105 @@ use CoryKeane\Slack\Client;
 class SlackController {
 	function __construct() {
 		echo "Inside of slack controller\n";
-		$config = [
-		    'token' => 'xoxp-1058876890481-1058876890769-1079248565984-e23b1cfb5d8ba3610cd693220f8c46c7',
-		    // 'team' => 'YOUR-TEAM',
-		    'username' => 'grittool',
-		    'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
-		    'parse' => '', // __construct function in Client.php calls for the parse parameter 
-		];
+		$config = include(__dir__.'/config/config.php');
 
 		$this->slack = new Client($config);
 		$this->chat = $this->slack->chat('#general');
 	}
 
-	public function sendChatMsg($msg) {
-		$this->chat->send($msg);
+	public function run($input) {
+		debug("Input is:".print_r($input, true));
+
+		if (isset($input['post_message'])) {
+			$this->sendBlockChatMsg("Check out the kit yooo");
+		}
+		elseif (isset($input['response_url'])) {
+			$this->dispatchEvent($input['response_url']);
+		}
+		elseif (isset($input['payload'])) {
+			$this->handlePayload(json_decode($input['payload']));
+		}
+
+		$this->listen();
+	}
+
+	public function sendChatMsg($msg, $blocks = null) {
+		$this->chat->send($msg, null, $blocks);
+	}
+
+	public function sendBlockChatMsg($msg) {
+		$blocks = [
+		    [
+		        'type' => 'section',
+		        'text' => [
+		            'type' => 'mrkdwn',
+		            'text' => 'This is a section block',
+		        ],
+		        'accessory' => [
+		            'type' => 'button',
+		            'text' => [
+		                'type' => 'plain_text',
+		                'text' => 'Click Me',
+		            ],
+		            'value' => 'click_me_123',
+		            'action_id' => 'button',
+		        ],
+		    ],
+		    [
+		        'type' => 'actions',
+		        'block_id' => 'actionblock789',
+		        'elements' => [
+		            [
+		                'type' => 'button',
+		                'text' => [
+		                    'type' => 'plain_text',
+		                    'text' => 'Primary Button',
+		                ],
+		                'style' => 'primary',
+		                'value' => 'click_me_456',
+		            ],
+		            [
+		                'type' => 'button',
+		                'text' => [
+		                    'type' => 'plain_text',
+		                    'text' => 'Link Button',
+		                ],
+		                'url' => 'https://api.slack.com/block-kit',
+		            ]
+		        ],
+		    ]
+		];
+
+		$this->sendChatMsg($msg, $blocks);
+	}
+
+	public function dispatchEvent($event) {
+
+	}
+
+	public function listen() {
+		error_log("BRN Listen");
+		$incoming = $this->slack->listen();
+		if ($incoming)
+		{
+			error_log("BRN incoming");
+		    switch($incoming->text())
+		    {
+		        case "What time is it?":
+		            $incoming->respond("It is currently ".date('g:m A T'));
+		        break;
+		        default:
+		            $incoming->respond("I don't understand what you're asking.");
+		        break;
+		    }
+		}
+
+		error_log("BRN incoming done");
+	}
+
+	public function handlePayload($payload) {
+		debug($payload);
+		$actions = $payload->actions;
+		$this->sendChatMsg("hey <@U011QRSS6NM>, ".$actions[0]->value);
 	}
 }
--- server/htdocs/slackapi.php
+++ server/htdocs/slackapi.php
@@ -4,16 +4,30 @@ error_log("BRN Slack API");
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (!isset($data["challenge"])) {
-    error_log(print_r($data, true)); 
-    require __dir__."/../SlackController.php";
-    $slackController = new SlackController();
-    $slackController->sendChatMsg("Hello Response from API");
+function debug($msg) {
+	$bt = debug_backtrace();
+	$caller = array_shift($bt);
+
+	$file = explode('/', $caller['file']);
+	$file = end($file);
+	$line = $caller['line'];
+	error_log("$file:$line-- ".print_r($msg, true));
 }
 
-else {
-
-    header("Content-Type: text/plain");
-    header('X-PHP-Response-Code: 200', true, 200);
-    echo $data["challenge"];
+if (isset($data["challenge"])) {
+	header("Content-Type: text/plain");
+	header('X-PHP-Response-Code: 200', true, 200);
+	file_put_contents('php://output', $data["challenge"]);
+	return;
 }
+
+$input = array_merge($data ?: [], $_GET ?: []);
+if (empty($input) || !$input){
+	return;
+}
+
+$input = ['payload' => '{"type":"block_actions","user":{"id":"U011QRSS6NM","username":"briandle89","name":"briandle89","team_id":"T011QRSS6E5"},"api_app_id":"A011FNUEY10","token":"MkLM99j5BsHuhYCdyKApK5aQ","container":{"type":"message","message_ts":"1586839078.000200","channel_id":"C011QRSSHBK","is_ephemeral":false},"trigger_id":"1059682809253.1058876890481.a2f370be7443c497605d3163c7da321a","team":{"id":"T011QRSS6E5","domain":"samus-workspace"},"channel":{"id":"C011QRSSHBK","name":"general"},"message":{"bot_id":"B011Q4W9Q7M","type":"message","text":"Check out the kit yooo","user":"U011QRSS6NM","ts":"1586839078.000200","team":"T011QRSS6E5","blocks":[{"type":"section","block_id":"4LvHY","text":{"type":"mrkdwn","text":"This is a section block","verbatim":false},"accessory":{"type":"button","action_id":"button","text":{"type":"plain_text","text":"Click Me","emoji":true},"value":"click_me_123"}},{"type":"actions","block_id":"actionblock789","elements":[{"type":"button","action_id":"oHs","text":{"type":"plain_text","text":"Primary Button","emoji":true},"style":"primary","value":"click_me_456"},{"type":"button","action_id":"jIk","text":{"type":"plain_text","text":"Link Button","emoji":true},"url":"https:\/\/api.slack.com\/block-kit"}]}]},"response_url":"https:\/\/hooks.slack.com\/actions\/T011QRSS6E5\/1059682809173\/RwswrNGGhXe7toCMfc6szypC","actions":[{"action_id":"button","block_id":"4LvHY","text":{"type":"plain_text","text":"Click Me","emoji":true},"value":"click_me_123","type":"button","action_ts":"1586839121.455937"}]}'];
+
+require __dir__."/../SlackController.php";
+$slackController = new SlackController();
+$slackController->run($input);
